73502999a84f40268f568c7a675216c45bce0cd2
catalina1306 <catalina.cepeda@ug.uchile.cl>
2023-10-02 13:23:24 -0300

    agrege una interfaz paara seleccion de personajes / no terminada

---
+++ TALLER/Scripts/main_select_Character_scene.gd
@@ -0,0 +1,13 @@
+extends Control
+@onready var fight = $VBoxContainer/Fight
+@onready var back = $VBoxContainer2/back
+
+func _ready() -> void:
+	fight.pressed.connect(_on_fight_pressed)
+	back.pressed.connect(_on_return_pressed)
+
+func _on_fight_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	
+func _on_return_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
