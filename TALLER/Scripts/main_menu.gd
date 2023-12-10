21030d0a96ea97e1ebcf2e05ca57763885fc37b9
catalina1306 <142618152+catalina1306@users.noreply.github.com>
2023-12-10 13:49:47 -0300

    credits

--- TALLER/Scripts/main_menu.gd
+++ TALLER/Scripts/main_menu.gd
@@ -1,13 +1,17 @@
 extends Control
 @onready var start = %Start
 @onready var exit = %Exit
-@onready var credits = %Credits
+@onready var credits = $VBoxContainer/Credits
 
 
 func _ready() -> void:
 	global.playMenuMusic()
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
+	credits.pressed.connect(_on_credits_pressed)
+	
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_select_character1.tscn")
