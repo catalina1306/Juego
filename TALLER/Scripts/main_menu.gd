0864694b7ea3e4b790494777cb83cb5ae71df3ef
catalina1306 <142618152+catalina1306@users.noreply.github.com>
2023-12-10 21:58:41 -0300

    muchas cosas

--- TALLER/Scripts/main_menu.gd
+++ TALLER/Scripts/main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 @onready var start = %Start
 @onready var exit = %Exit
 @onready var credits = $VBoxContainer/Credits
+@onready var tutorial = $VBoxContainer/Tutorial
+
 
 
 func _ready() -> void:
@@ -9,6 +11,7 @@ func _ready() -> void:
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
 	credits.pressed.connect(_on_credits_pressed)
+	tutorial.pressed.connect(_on_tutorial_pressed)
 	
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
@@ -19,3 +22,5 @@ func _on_start_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 	
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
--- TALLER/Scripts/tutorial.gd
+++ TALLER/Scripts/tutorial.gd
@@ -1,17 +1,26 @@
 extends Node2D
-
+@onready var players = $players
 
 @onready var personaje1 = load(global.character1).instantiate()
 @onready var personaje2 = load(global.character2).instantiate()
+
 @onready var pos_1 = $pos1
-@onready var players = $players
+@onready var pos_2 = $pos2
 @export var scene: String
+@onready var skip = $Skip
 
 
 func _ready():
 	global.playFightMusic()
+	skip.pressed.connect(_on_skip_pressed)
 	personaje1.global_position = pos_1.global_position
+	personaje2.global_position = pos_2.global_position
 	players.add_child(personaje1)
+	players.add_child(personaje2)
 	personaje1.index = 1
+	personaje2.index = 2
 	global.scene_dir=scene
 
+func _on_skip_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_select_character1.tscn")
+	
